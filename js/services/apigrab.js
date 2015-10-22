5b1785004a8760588582321dddb70229bf3ea83e
CSEBTCOR <shack4@btcor.co>
2015-10-21 23:24:14 -0400

    Create apigrab.js

---
+++ js/services/apigrab.js
@@ -0,0 +1,14 @@
+var userAPIToken = "xxxxx";
+var userAPIAccountURL = "https://1broker.com/api/v1/account/info.php?token="+userAPIToken+"&pretty=1";
+var marketList = "https://1broker.com/api/v1/market/list.php?token="+userAPIToken+"&pretty=1";
+var marketSymbol = "EURUSD";
+var marketPrice = "https://1broker.com/api/v1/market/quotes.php?symbols="+marketSymbol+"&token="+userAPIToken+"&pretty=1";
+app.factory('apiMarketGrab', ['$http', function($http) {
+    return $http.get(marketList)
+        .success(function(data) {
+            return data;
+        })
+        .error(function(err) {
+            return err;
+        });
+}]);
